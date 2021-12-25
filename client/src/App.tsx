41fc18ef459a6d349f469c598c16ae8124d12dc9
YusufZiyaOzgul <yzozgul98@gmail.com>
2021-12-25 22:22:52 +0300

    Upload Image.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 
 import './App.css';
-import Button from '@mui/material/Button'
+import Button from '@mui/material/Button';
 import OmAppBar from './components/OmAppBar';
 import OmAppBar2 from './components/OmAppBar2';
 import OmMapBox from './components/OmMapBox';
 import Grid from '@mui/material/Grid';
 import Item from '@mui/material/Grid'
+import SegPage from './SegPage';
+import UploadImages from "./upload-images.component";
+
 
 function App() {
   return (
     <div>
      
         <OmAppBar2/> 
+        <SegPage/>
+        
        
-        <OmMapBox/>
     
       
     </div>
   );
+   /*<OmMapBox/>*/
 }
 
 export default App;
