cbd8a2de535d73c3706b47d63d377af6bf15e365
atakan <atakanaray@gmail.com>
2021-12-25 21:33:46 +0300

    router added

---
+++ client/src/pages/Segmentationpage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import OmAppBar2 from '../components/OmAppBar2';
+import OmMapBox from '../components/OmMapBox';
+import OmAppBar from '../components/OmAppBar';
+
+function SegmentationPage() {
+    return(
+        <div>
+        <OmAppBar2/> 
+    </div>
+    )
+
+
+}
+
+export default SegmentationPage
