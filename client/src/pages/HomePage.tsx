1125ddac948c8f6d60802005c5e519c16ceffaee
atakan <atakanaray@gmail.com>
2021-12-28 01:28:39 +0300

    a

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import OmAppBar2 from '../components/OmAppBar2';
 
+
 function HomePage(){
     return(
         
         <div style={{ display: "flex", justifyContent: "flex-end", flexDirection:'column' }}>
-            <OmAppBar2/>
-            <img src='../images/info.jpg' alt='a'> </img>
+            <OmAppBar2/> 
         </div>
     )
 }
