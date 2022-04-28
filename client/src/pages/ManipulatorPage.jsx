c20e8d8634d1daaabea75e81668a0b316abd299a
Batuhan Ozcomlekci <b.ozcomlekci@ug.bilkent.edu.tr>
2022-04-29 02:28:48 +0300

    Manipulator (Alternative)

---
+++ client/src/pages/ManipulatorPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import OmAppBar2 from "../components/OmAppBar2";
+import Manipulator from "../components/Manipulator";
+
+export default function ManipulatorPage () {
+
+    return(
+        <div className="ManipulatorPage">
+            <OmAppBar2/> 
+            <Manipulator/>
+        </div>
+    )
+}
